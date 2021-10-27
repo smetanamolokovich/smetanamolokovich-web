ad9febc6b0e359004ac0b21c0f10c10c1ce1da35
Aituar <aituufs@gmail.com>
2021-10-27 23:11:14 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,113 @@
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Heading,
+  Image,
+  Button,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioYear, BioSection } from '../components/bio'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        mt={6}
+        align="center"
+      >
+        Hello, I am a front-end developer based in Prague!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Aituar
+          </Heading>
+          <p>Dev stack: JS/TS, React, Vue, Node, MongoDB</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whieAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/cow.jpg"
+            alt="Profile Image"
+          />
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          dignissim arcu arcu, eu tempor urna dapibus in. Nulla arcu arcu,
+          blandit et sodales et, mattis porttitor tortor. Fusce lobortis lacinia
+          molestie. Integer ultrices nisl sed semper auctor. Duis rutrum, lectus
+          at cursus placerat, lacus turpis ullamcorper lacus, at rutrum tellus
+          odio dapibus ex{' '}
+          <NextLink href="/works/pagination">
+            <Link>Paginator</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" mt={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1998</BioYear>
+          Born in Altay, CHN.
+        </BioSection>
+        <BioSection>
+          <BioYear>2019</BioYear>
+          Student in 2nd academic year, in Czech University of Life Science in
+          Prague, faculty of Econimics, Informatics pragramme.
+        </BioSection>
+        <BioSection>
+          <BioYear>2014</BioYear>
+          Arrived to Prague, CR
+        </BioSection>
+      </Section>
+
+      <Section>
+        <Heading as="h3" variant="section-title">
+          What I like to do
+        </Heading>
+        <Paragraph>
+          Coding, Basketball, Cooking, Learning new stuff, Reading, Backend
+          development, Blockchain
+        </Paragraph>
+      </Section>
+    </Container>
+  )
+}
+
+export default Page
---
+++ pages/works.js
@@ -0,0 +1,33 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbnailPaginator from '../public/images/thumbnail.png'
+import thumbnailSnippetBox from '../public/images/snippetbox.png'
+
+const Works = () => {
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="paginator" title="Paginator" thumbnail={thumbnailPaginator}>
+            Telebot library extention. Telebot is Telegram bot library build in
+            Golang.
+          </WorkGridItem>
+        </Section>
+
+		<Section>
+          <WorkGridItem id="snippetbox" title="SnippetBox" thumbnail={thumbnailSnippetBox}>
+			SnippetBox lets people paste and share snippets of text.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  )
+}
+
+export default Works
